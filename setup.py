8b10114b17643afe8bbc79898cc4d1b542dab6b1
mmurdoch <matthew.murdoch.0@gmail.com>
2013-11-14 21:48:48 +0000

    Get setup.py ready for v1.2.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='Vengeance',
-    version='1.1.1',
+    version='1.2.0',
     author='Matthew Murdoch',
     author_email='matthew.murdoch.0@gmail.com',
     packages=['vengeance', 'vengeance.test'],
-    scripts=['bin/declaratively_defined_game.py', 'bin/procedurally_defined_game.py', 'bin/embed_engine.py', 'bin/game_testing.py'],
+    scripts=['bin/declaratively_defined_game.py', 'bin/procedurally_defined_game.py', 'bin/embed_engine.py', 'bin/game_testing.py', 'bin/game_ending.py'],
     url='http://pypi.python.org/pypi/Vengeance/',
     license='LICENSE.txt',
     description='Text-based adventure game engine',
