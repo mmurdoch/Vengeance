e8e9ff5c3fdf5353f9388c1ed6d26a86ca0b0879
mmurdoch <matthew.murdoch.0@gmail.com>
2013-11-20 08:48:36 +0000

    Prepare for release v1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Vengeance',
-    version='1.3.0',
+    version='1.3.1',
     author='Matthew Murdoch',
     author_email='matthew.murdoch.0@gmail.com',
     packages=['vengeance', 'vengeance.test'],
